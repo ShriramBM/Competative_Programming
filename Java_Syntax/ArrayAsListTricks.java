2e95a298f9e7c39e59530e2675d06787eac8b08b
ShriramBM <shrirambm007@outlook.com>
2022-11-06 23:22:20 +0530

    06-11-2022

--- Java_Syntax/ArrayAsListTricks.java
+++ Java_Syntax/ArrayAsListTricks.java
@@ -1,15 +1,12 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
 
 public class ArrayAsListTricks {
     public static void main(String[] args) {
         List<Integer> t = new ArrayList<>();
         List<Integer> x = Arrays.asList(100);
  
-       
-
     
         System.out.println(x+" "+t);
         
